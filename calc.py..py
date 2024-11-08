2a5c99279b476f9682e84f8cc21dfb2323795293
nassirosman <96050460+nassirosman@users.noreply.github.com>
2024-11-08 14:21:07 +0000

    Assessment 2

---
+++ calc.py..py
@@ -0,0 +1,12 @@
+num1 =float(input("enter the fist number: "))
+num2 =float(input("enter the second number: "))
+
+sum_result = num1 + num2
+difference = num1 - num2
+product = num1 * num2
+quotient = num1 / num2
+
+print("sum:",num1 + num2)
+print("difference:",num1 - num2)
+print("product:",num1 * num2)
+print("quotient:", num1 / num2)
