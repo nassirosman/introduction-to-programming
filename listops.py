a509a631b8acf55b076123be9b71017d1f3da7ed
nassirosman <96050460+nassirosman@users.noreply.github.com>
2024-12-13 14:09:23 +0000

    Assessment 6
     I tried my best to complete A6 but i was not able too.

---
+++ listops.py
@@ -0,0 +1,15 @@
+numbers = [15, 4, 34, 85, 48, 57, 27, 1, 6, 8,]
+def print_list():
+    print("list:")
+
+def print_sum():
+    print("sum of elements:")
+
+def print_largest():
+    print("largest element:")
+
+def print_reverse():
+    print("reversed list:")
+
+def print_smallest():
+    print("smallest element:")
