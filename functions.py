ed79868642813fcf1b16cfc5fdaa827c16551c17
nassirosman <96050460+nassirosman@users.noreply.github.com>
2024-12-13 14:05:16 +0000

    assessment 5
    I tried my best to do this but i was not able to get it working

---
+++ functions.py
@@ -0,0 +1,5 @@
+def convert_temperature(value conversion_type):
+    if conversion_type == "C_to_F":
+        return (value * 9/5) + 32
+    elif conversion_type == "F_to_C"
+    return (value - 32) * 5/9 
