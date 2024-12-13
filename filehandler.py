6f323959c8bd8ac5cdd217d46b686e26d07d15e3
nassirosman <96050460+nassirosman@users.noreply.github.com>
2024-12-13 15:24:48 +0000

    Assessment 7
    This is my best attempt

---
+++ filehandler.py
@@ -0,0 +1,14 @@
+def calculate_weighted_grade():
+    input_file = "input.txt"
+    output_file = "output.txt"
+    with open(input_file, 'r') as infile, open(output_file, 'w') as outfile:
+            outfile.write("Name, Mark, Grade\n")
+            
+    
+
+ 
+               
+ 
+            
+
+    
